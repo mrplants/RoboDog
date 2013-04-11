0b481bdb19437d8cf6dbec8442526fe99d73f681
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2013-04-10 21:35:47 -0300

    Create main.cpp
    Created and initialized the main.cpp. Main has arguments for SDL.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//Sean Fitzgerald, Jon Gautsch, Daniel Tamaru, Maribeth Rauh
+//Final Project CSE 20212 Spring 2013
+//Main.cpp - used to initialize the Master UI and Game Control class
+
+
+#include <iostream>
+
+int main (int argc, char * args[])
+{
+  //create an instance of the master game class
+
+  //tell the master game class instance to play
+
+  return 0;
+}
