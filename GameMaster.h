fb1cc9ca510f9203f1d2075b4c59deae9f16acf8
twistedTightly <mrauh1@nd.edu>
2013-04-14 16:43:46 -0400

    SDL and token related data members/functions added to GameMaster

--- GameMaster.h
+++ GameMaster.h
@@ -1,8 +1,17 @@
-//Sean T Fitzgerald, Jon T Gautsch, Daniel Y Tamaru, Maribeth E Rauh
-//master game controller - links together the game classes
-//has the game classes composed into it
+/*
+ GameMaster.h
+ 
+ Sean T Fitzgerald, Jon T Gautsch, Daniel Y Tamaru, Maribeth E Rauh
+
+ Master Game Controller - links together all the game classes via composition
+ 
+ */
+
+#ifndef GAME_MASTER
+#define GAME_MASTER
 
 /*
+#include "SDL_Program.h" //base class for GameMaster.h
 #include //Maribeth's classes
 #include //Jon's classes
 #include //Sean's classes
@@ -34,11 +43,19 @@ class GameMaster {
     //show welcome screen
     //show high scores
 
-    SDL_Surface * screen;
-    SDL_Surface * background;
+    SDL_Surface * screen; //screen or canvas all images are blit onto
+    SDL_Surface * background; //whatever fills the whole screen's background
+    SDL_Rect snapRegion; //rectangle that defines the area an image will snap to
+    SDL_Surface *snapImage; //visual representation of the snap region (probably not needed for final project)
+    
+    void setUpSnapRegion(int, int, SDL_Surface *); //adds a rect to the screen that will  be used to snap tokens into place
+    void applySnapRegion(SDL_Surface *); //adds a visual representation of the snap region to the screen
+    SDL_Surface* createBlankSurface(Uint32 flags, int width, int height, const SDL_Surface* screen); //creates the blank surface applied to the screen by applySnapRegion
 
     //interpreter
     //token queue
     //game world
 
 };
+
+#endif // defined GAME_MASTER
