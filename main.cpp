bbc3ffcbc4e2aecfb1c8c384dc33e51c87dadc24
twistedTightly <mrauh1@nd.edu>
2013-04-14 19:15:17 -0400

    GameMaster object instantiated in main, play() called

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
-//Sean Fitzgerald, Jon Gautsch, Daniel Tamaru, Maribeth Rauh
-//Final Project CSE 20212 Spring 2013
-//Main.cpp - used to initialize the Master UI and Game Control class
+/*
+ Sean Fitzgerald, Jon Gautsch, Daniel Tamaru, Maribeth Rauh
+ 
+ Final Project CSE 20212 Spring 2013
+ 
+ Main.cpp - used to initialize the Master UI and Game Control class
+ */
 
 
 #include <iostream>
+#include "GameMaster.h"
 
 int main (int argc, char * args[])
 {
   //create an instance of the master game class
+    GameMaster testGame;
 
   //tell the master game class instance to play
+    testGame.play();
 
   return 0;
 }
