317788bd49668d8d0d088ec16fa371934536163b
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2013-04-21 17:38:03 -0400

    added extern globals in the constants file

--- DanMario/Constants.h
+++ DanMario/Constants.h
@@ -1,5 +1,34 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+ extern const int SCREEN_WIDTH = 640;
+ extern const int SCREEN_HEIGHT = 480;
+extern  const int SCREEN_BPP = 32;
+
+//The frame rate
+ extern const int FRAMES_PER_SECOND = 20;
+
+//The dimensions of Mario
+ extern const int MARIO_WIDTH = 184;
+ extern const int MARIO_HEIGHT = 200;
+
+//The action status of Mario
+ extern const int MARIO_STAND = 0;
+ const int MARIO_JUMP = 1;
+
+//The surfaces
+ extern SDL_Surface *mario = NULL;
+ extern SDL_Surface *background = NULL;
+ extern SDL_Surface *screen = NULL;
+
+//The event structure
+ extern SDL_Event event;
+
+//The areas of the sprite sheet
+ extern SDL_Rect clips[ 2 ];
+
+// tells us if we are scrolling (1 if yes, 0 if no)                                                               
+ extern int scrolling;
+
 
 #endif
