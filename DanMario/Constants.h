f80f1a29e44e85e67d802154d08ddac426c45c53
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2013-04-21 16:58:37 -0400

    removed extern from constants.h

--- DanMario/Constants.h
+++ DanMario/Constants.h
@@ -1,33 +1,33 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-extern const int SCREEN_WIDTH = 640;
-extern const int SCREEN_HEIGHT = 480;
-extern const int SCREEN_BPP = 32;
+ const int SCREEN_WIDTH = 640;
+ const int SCREEN_HEIGHT = 480;
+ const int SCREEN_BPP = 32;
 
 //The frame rate
-extern const int FRAMES_PER_SECOND = 20;
+ const int FRAMES_PER_SECOND = 20;
 
 //The dimensions of Mario
-extern const int MARIO_WIDTH = 184;
-extern const int MARIO_HEIGHT = 200;
+ const int MARIO_WIDTH = 184;
+ const int MARIO_HEIGHT = 200;
 
 //The action status of Mario
-extern const int MARIO_STAND = 0;
-extern const int MARIO_JUMP = 1;
+ const int MARIO_STAND = 0;
+ const int MARIO_JUMP = 1;
 
 //The surfaces
-extern SDL_Surface *mario = NULL;
-extern SDL_Surface *background = NULL;
-extern SDL_Surface *screen = NULL;
+ SDL_Surface *mario = NULL;
+ SDL_Surface *background = NULL;
+ SDL_Surface *screen = NULL;
 
 //The event structure
-extern SDL_Event event;
+ SDL_Event event;
 
 //The areas of the sprite sheet
-extern SDL_Rect clips[ 2 ];
+ SDL_Rect clips[ 2 ];
 
 // tells us if we are scrolling (1 if yes, 0 if no)                                                               
-extern int scrolling;
+ int scrolling;
 
 #endif
