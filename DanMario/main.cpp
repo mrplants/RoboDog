94da7d2c10164d51fc62d1e87215659e4848e267
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2013-04-21 17:46:03 -0400

    more constants trouble...

--- DanMario/main.cpp
+++ DanMario/main.cpp
@@ -5,35 +5,6 @@
 #include "Background.h"
 #include "Constants.h"
 
- const int SCREEN_WIDTH = 640;
- const int SCREEN_HEIGHT = 480;
- const int SCREEN_BPP = 32;
-
-//The frame rate
- const int FRAMES_PER_SECOND = 20;
-
-//The dimensions of Mario
- const int MARIO_WIDTH = 184;
- const int MARIO_HEIGHT = 200;
-
-//The action status of Mario
- const int MARIO_STAND = 0;
- const int MARIO_JUMP = 1;
-
-//The surfaces
- SDL_Surface *mario = NULL;
- SDL_Surface *background = NULL;
- SDL_Surface *screen = NULL;
-
-//The event structure
- SDL_Event event;
-
-//The areas of the sprite sheet
- SDL_Rect clips[ 2 ];
-
-// tells us if we are scrolling (1 if yes, 0 if no)                                                               
- int scrolling;
-
 
 SDL_Surface *load_image( std::string filename )
 {
@@ -163,6 +134,7 @@ void clean_up()
 //main function
 int main( int argc, char* args[] )
 {
+
   //Quit flag
   bool quit = false;
 
