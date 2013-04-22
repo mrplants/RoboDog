a1d96c7d0f4d37f756299ec97f4f1c2c893831cc
Daniel Tamaru <datamaru@dhcp-10-18-56-209.dhcp.nd.edu>
2013-04-22 03:03:24 -0400

    moved DanMario files

---
+++ Mario.h
@@ -0,0 +1,23 @@
+#include "Defines.h"
+
+class Mario
+{
+ private:
+  SDL_Rect marioRect;
+
+  //Its current frame                                                        
+  int frame;
+
+  //Its animation status                                                      
+  int status;
+
+ public:
+  //Initializes the variables                                                 
+  Mario();
+
+  //Shows Mario                                                              
+  void show();
+
+  //Mario surface                                                                                                                                                 
+  SDL_Surface *marioSurface;
+};
