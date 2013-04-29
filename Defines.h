9b6fa6dff1db1281f4aeef73ba187277d70ce9b0
Daniel Tamaru <datamaru@dhcp-10-18-34-127.dhcp.nd.edu>
2013-04-28 20:41:14 -0400

    changed defines

--- Defines.h
+++ Defines.h
@@ -3,16 +3,13 @@
 #include <string>
 
 //Screen attributes
-#define SCREEN_WIDTH = 640;
-#define SCREEN_HEIGHT = 480;
-#define SCREEN_BPP = 32;
-
-//The frame rate             
-//#define FRAMES_PER_SECOND 20;
+#define SCREEN_WIDTH 640;
+#define SCREEN_HEIGHT 480;
+#define SCREEN_BPP 32;
 
 //The dimensions of Mario 
-#define MARIO_WIDTH = 181;
-#define MARIO_HEIGHT = 123;
+#define MARIO_WIDTH 181;
+#define MARIO_HEIGHT 123;
 
 //The action status of Mario
 #define MARIO_STAND 0;
