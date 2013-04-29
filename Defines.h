f97b0d045053164b1b465e4afcd2c33aa394e6a1
Daniel Tamaru <datamaru@dhcp-10-18-34-127.dhcp.nd.edu>
2013-04-28 20:38:33 -0400

    changed defines

--- Defines.h
+++ Defines.h
@@ -8,11 +8,11 @@
 #define SCREEN_BPP = 32;
 
 //The frame rate             
-#define FRAMES_PER_SECOND 20;
+//#define FRAMES_PER_SECOND 20;
 
 //The dimensions of Mario 
-#define MARIO_WIDTH 181;
-#define MARIO_HEIGHT 123;
+#define MARIO_WIDTH = 181;
+#define MARIO_HEIGHT = 123;
 
 //The action status of Mario
 #define MARIO_STAND 0;
