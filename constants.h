7dffe268acb656d9d509cf06ec8549982325d0af
Daniel Tamaru <datamaru@dhcp-10-18-37-156.dhcp.nd.edu>
2013-04-30 03:09:16 -0400

    redefining constants

--- constants.h
+++ constants.h
@@ -11,7 +11,7 @@
     #define TOKEN_W (104)
     #define TOKEN_H (39)
     
-    #define TOKEN_PANE_PERCENT_W (176/640)
+    #define TOKEN_PANE_PERCENT_W 176/640
     #define TOKEN_PANE_W 176
     #define TOKEN_PANE_PERCENT_H 1
     //the height of the token pane is the height of the screen
@@ -23,7 +23,7 @@
     
     #define TOKEN_LIB_PERCENT_W (1-TOKEN_PANE_PERCENT_W)
     #define TOKEN_LIB_W 464 
-    #define TOKEN_LIB_PERCENT_H (150/640) 
+    #define TOKEN_LIB_PERCENT_H 150/640 
     #define TOKEN_LIB_H 150 
     #define TOKEN_LIB_START_X (640-TOKEN_LIB_W)
  
