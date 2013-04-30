98ff2b1c66fec9d17ac90868482def8acf045353
Daniel Tamaru <datamaru@dhcp-10-18-37-156.dhcp.nd.edu>
2013-04-30 02:41:20 -0400

    debugging animation

--- GameAnimation.cpp
+++ GameAnimation.cpp
@@ -31,7 +31,7 @@ void GameAnimation::clip_background() {
 
   // clip the portions of the background
   for (int i = 0; i < (LEVEL_LENGTH / GAME_WORLD_W * 8); i++) { 
-    backClips[ i ].x = i * (LEVEL_LENGTH / GAME_WORLD_W / 8);
+    backClips[ i ].x = i * (LEVEL_LENGTH / GAME_WORLD_W % 8);
     backClips[ i ].y = 0;
     backClips[ i ].w = GAME_WORLD_W;
     backClips[ i ].h = GAME_WORLD_H;
